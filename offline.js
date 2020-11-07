81fe2bd58c3af2e8459d104f6246b00a34f9ebd3
ECKLE Elias <e.eckle@ludus-acdemie.com>
2020-11-07 18:13:45 +0100

    initial commit

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1604767750,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/persprincipal-sheet0.png",
+		"images/persprincipal-sheet1.png",
+		"images/pnj1-sheet0.png",
+		"images/pnj1-sheet1.png",
+		"images/pnj2-sheet0.png",
+		"images/sol-sheet0.png",
+		"images/tiledbackground.png",
+		"images/boutonx_dialogue-sheet0.png",
+		"images/boutonx_dialogue-sheet1.png",
+		"images/boitedialogue-sheet0.png",
+		"images/panneauoption1-sheet0.png",
+		"images/flecheb-sheet0.png",
+		"images/boutono-sheet0.png",
+		"media/ambianceenquete.ogg",
+		"media/bruitpaspluspluscourt.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
